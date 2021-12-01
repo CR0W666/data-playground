e1bc734177613048935c196e130f0a02df61a3c2
CR0W <44005888+CR0W666@users.noreply.github.com>
2021-12-01 19:21:42 +0100

    Formatting

--- src/main/java/streams/DataProcessor.java
+++ src/main/java/streams/DataProcessor.java
@@ -8,24 +8,36 @@ import java.util.Optional;
 public class DataProcessor {
 
     public static boolean atLeastOneGradeA(Student student) {
+
         for (Grade grade : student.getGrades()) {
-            if(grade.getType() == GradeType.A) return true;
+
+            if (grade.getType() == GradeType.A)
+                return true;
         }
         return false;
     }
 
 
     public static List<Integer> getStudentAges(List<Student> students) {
-        List<Integer> ages = new ArrayList<>();
-        for(Student student : students) ages.add(student.getAge());
+        final List<Integer> ages = new ArrayList<>();
+
+        for (Student student : students) {
+
+            ages.add(student.getAge());
+        }
 
         return ages;
     }
 
     public static List<Student> getStudentsWithMinimumAge(List<Student> students, int minAge) {
-        List<Student> minAgeStudents = new ArrayList<>();
-        for(Student student : students) if(minAge == student.getAge()) minAgeStudents.add(student);
-        
+        final List<Student> minAgeStudents = new ArrayList<>();
+
+        for (Student student : students) {
+
+            if (minAge == student.getAge())
+                minAgeStudents.add(student);
+        }
+
         return minAgeStudents;
     }
 
@@ -34,7 +46,12 @@ public class DataProcessor {
     // or getGender().name().equals("MALE")
     public static long countMaleStudents(List<Student> students) {
         long maleStudents = 0;
-        for(Student student : students) if(student.getGender().compareTo(Gender.MALE) == 0) maleStudents++;
+
+        for (Student student : students) {
+
+            if (student.getGender().compareTo(Gender.MALE) == 0)
+                maleStudents++;
+        }
 
         return maleStudents;
     }
@@ -43,26 +60,41 @@ public class DataProcessor {
     public static double avgAgeOfFemaleStudent(List<Student> students) {
         int femaleAgesTotal = 0;
         int numOfFemaleStudents = 0;
-        for(Student student : students) {
-            if(student.getGender().compareTo(Gender.FEMALE) == 0) { 
-                femaleAgesTotal += student.getAge(); numOfFemaleStudents++; 
+
+        for (Student student : students) {
+
+            if (student.getGender().compareTo(Gender.FEMALE) == 0) {
+                femaleAgesTotal += student.getAge();
+                numOfFemaleStudents++;
             }
         }
-        if(numOfFemaleStudents == 0) return 0.0;
+
+        if (numOfFemaleStudents == 0)
+            return 0.0;
 
         return (double) femaleAgesTotal / numOfFemaleStudents;
     }
 
     public static Integer getProductOfStudentAges(List<Student> students) {
         int productOfStudentAges = 1;
-        for(Student student : students) productOfStudentAges *= student.getAge();
+
+        for (Student student : students) {
+            productOfStudentAges *= student.getAge();
+        }
+
         return productOfStudentAges;
     }
 
     // ignore F Grades
     public static double productOfStudentGrades(Student student) {
         double productOfStudentGrades = 1.0;
-        for(Grade grade : student.getGrades()) if(grade.getType() != GradeType.F) productOfStudentGrades *= grade.getType().getValue();
+
+        for (Grade grade : student.getGrades()) {
+
+            if (grade.getType() != GradeType.F)
+                productOfStudentGrades *= grade.getType().getValue();
+        }
+
         return productOfStudentGrades;
     }
 
@@ -70,8 +102,12 @@ public class DataProcessor {
 
     public static Optional<Grade> getBestMathGradeFromStudent(Student student) {
         Grade bestMathGrade = null;
-        for(Grade grade : student.getGrades()) {
-            if(grade.getSubject().compareTo(Subject.MATH) == 0 && (bestMathGrade == null || bestMathGrade.compareTo(grade) < 0)) {
+
+        for (Grade grade : student.getGrades()) {
+
+            if (grade.getSubject().compareTo(streams.Subject.MATH) == 0
+                    && (bestMathGrade == null || bestMathGrade.compareTo(grade) < 0)) {
+
                 bestMathGrade = grade;
             }
         }
@@ -80,10 +116,14 @@ public class DataProcessor {
     }
 
     public static List<Integer> getSortedAges(List<Student> students) {
-        List<Integer> ages = new ArrayList<>();
-        for(Student student : students) ages.add(student.getAge());
+        final List<Integer> ages = new ArrayList<>();
+
+        for (Student student : students) {
+            ages.add(student.getAge());
+        }
+
         ages.sort(Comparator.naturalOrder());
-        
+
         return ages;
     }
 
