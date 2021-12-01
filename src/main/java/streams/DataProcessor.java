65d5a187dfb7e7752aba6dcce14ab4ce841c9b28
CR0W <44005888+CR0W666@users.noreply.github.com>
2021-12-01 19:02:02 +0100

    done, vse

--- src/main/java/streams/DataProcessor.java
+++ src/main/java/streams/DataProcessor.java
@@ -1,54 +1,90 @@
 package streams;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class DataProcessor {
 
-
     public static boolean atLeastOneGradeA(Student student) {
+        for (Grade grade : student.getGrades()) {
+            if(grade.getType() == GradeType.A) return true;
+        }
         return false;
     }
 
 
     public static List<Integer> getStudentAges(List<Student> students) {
-        return new ArrayList<>();
+        List<Integer> ages = new ArrayList<>();
+        for(Student student : students) ages.add(student.getAge());
+
+        return ages;
     }
 
     public static List<Student> getStudentsWithMinimumAge(List<Student> students, int minAge) {
-        return new ArrayList<>();
+        List<Student> minAgeStudents = new ArrayList<>();
+        for(Student student : students) if(minAge == student.getAge()) minAgeStudents.add(student);
+        
+        return minAgeStudents;
     }
 
 
     // getGender() == Gender.MALE
     // or getGender().name().equals("MALE")
     public static long countMaleStudents(List<Student> students) {
-        return 0;
+        long maleStudents = 0;
+        for(Student student : students) if(student.getGender().compareTo(Gender.MALE) == 0) maleStudents++;
+
+        return maleStudents;
     }
 
 
     public static double avgAgeOfFemaleStudent(List<Student> students) {
-        return 0.0;
+        int femaleAgesTotal = 0;
+        int numOfFemaleStudents = 0;
+        for(Student student : students) {
+            if(student.getGender().compareTo(Gender.FEMALE) == 0) { 
+                femaleAgesTotal += student.getAge(); numOfFemaleStudents++; 
+            }
+        }
+        if(numOfFemaleStudents == 0) return 0.0;
+
+        return (double) femaleAgesTotal / numOfFemaleStudents;
     }
 
     public static Integer getProductOfStudentAges(List<Student> students) {
-        return 0;
+        int productOfStudentAges = 1;
+        for(Student student : students) productOfStudentAges *= student.getAge();
+        return productOfStudentAges;
     }
 
     // ignore F Grades
     public static double productOfStudentGrades(Student student) {
-        return 0.0;
+        double productOfStudentGrades = 1.0;
+        for(Grade grade : student.getGrades()) if(grade.getType() != GradeType.F) productOfStudentGrades *= grade.getType().getValue();
+        return productOfStudentGrades;
     }
 
     // region BONUS
 
     public static Optional<Grade> getBestMathGradeFromStudent(Student student) {
-        return Optional.empty();
+        Grade bestMathGrade = null;
+        for(Grade grade : student.getGrades()) {
+            if(grade.getSubject().compareTo(Subject.MATH) == 0 && (bestMathGrade == null || bestMathGrade.compareTo(grade) < 0)) {
+                bestMathGrade = grade;
+            }
+        }
+
+        return Optional.ofNullable(bestMathGrade);
     }
 
     public static List<Integer> getSortedAges(List<Student> students) {
-        return new ArrayList<>();
+        List<Integer> ages = new ArrayList<>();
+        for(Student student : students) ages.add(student.getAge());
+        ages.sort(Comparator.naturalOrder());
+        
+        return ages;
     }
 
     // endregion
