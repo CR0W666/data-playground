f4649c75d46ed36fc5158eaa4eed73bec8927db1
CR0W666 <lukas.petracek@educanet.cz>
2022-02-23 16:01:05 +0100

    Finisheddd

--- src/main/java/streams/DataProcessor.java
+++ src/main/java/streams/DataProcessor.java
@@ -1,115 +1,57 @@
 package streams;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 public class DataProcessor {
-    //TODO
+
     public static boolean atLeastOneGradeA(Student student) {
-
-        for (Grade grade : student.getGrades()) {
-
-            if (grade.getType() == GradeType.A)
-                return true;
-        }
-        return false;
+        return student.getGrades().stream().map(Grade::getType).anyMatch(type -> type == GradeType.A);
     }
 
 
     public static List<Integer> getStudentAges(List<Student> students) {
-
-        return students.stream().mapToInt(Student::getAge).boxed().toList();
+        return students.stream().mapToInt(Student::getAge).boxed().collect(Collectors.toList());
     }
-    //TODO
+
     public static List<Student> getStudentsWithMinimumAge(List<Student> students, int minAge) {
-        final List<Student> minAgeStudents = new ArrayList<>();
-
-        for (Student student : students) {
-
-            if (minAge == student.getAge())
-                minAgeStudents.add(student);
-        }
-
-        return minAgeStudents;
+        return students.stream().filter(student -> student.getAge() == minAge).collect(Collectors.toList());
     }
 
 
     // getGender() == Gender.MALE
     // or getGender().name().equals("MALE")
-    //TODO
+
     public static long countMaleStudents(List<Student> students) {
-        long maleStudents = 0;
-
-        for (Student student : students) {
-
-            if (student.getGender().compareTo(Gender.MALE) == 0)
-                maleStudents++;
-        }
-
-        return maleStudents;
+        return (int) students.stream().filter(student -> student.getGender() == Gender.MALE).count();
     }
 
-    //TODO
+
     public static double avgAgeOfFemaleStudent(List<Student> students) {
-        int femaleAgesTotal = 0;
-        int numOfFemaleStudents = 0;
-
-        for (Student student : students) {
-
-            if (student.getGender().compareTo(Gender.FEMALE) == 0) {
-                femaleAgesTotal += student.getAge();
-                numOfFemaleStudents++;
-            }
-        }
-
-        if (numOfFemaleStudents == 0)
-            return 0.0;
-
-        return (double) femaleAgesTotal / numOfFemaleStudents;
+        return students.stream().filter(student -> student.getGender() == Gender.FEMALE).map(Student::getAge).reduce(0, Integer::sum);
     }
 
     public static Integer getProductOfStudentAges(List<Student> students) {
-        
         return students.stream().mapToInt(Student::getAge).reduce(1, (a,b) -> a* b);
     }
 
     // ignore F Grades
     //TODO
     public static double productOfStudentGrades(Student student) {
-        double productOfStudentGrades = 1.0;
-
-        for (Grade grade : student.getGrades()) {
-
-            if (grade.getType() != GradeType.F)
-                productOfStudentGrades *= grade.getType().getValue();
-        }
-
-        return productOfStudentGrades;
+        return student.getGrades().stream().mapToInt(grade -> grade.getType().getValue()).reduce(1, (a, b) -> a * b);
     }
 
     // region BONUS
-    //TODO
     public static Optional<Grade> getBestMathGradeFromStudent(Student student) {
-        Grade bestMathGrade = null;
-
-        for (Grade grade : student.getGrades()) {
-
-            if (grade.getSubject().compareTo(streams.Subject.MATH) == 0
-                    && (bestMathGrade == null || bestMathGrade.compareTo(grade) < 0)) {
-
-                bestMathGrade = grade;
-            }
-        }
-
-        return Optional.ofNullable(bestMathGrade);
+        return Optional.of(student.getGrades().stream().filter(grade -> grade.getSubject() == Subject.MATH).max(Comparator.comparing(Grade::getType)).get());
     }
 
     public static List<Integer> getSortedAges(List<Student> students) {
-
-
-        return students.stream().mapToInt(Student::getAge).sorted().boxed().toList();
+        return students.stream().mapToInt(Student::getAge).sorted().boxed().collect(Collectors.toList());
     }
 
     // endregion
--- src/main/kotlin/processing/data_processing.kt
+++ src/main/kotlin/processing/data_processing.kt
@@ -4,50 +4,50 @@ import streams.*
 
 
 fun atLeastOneGradeA(student: Student): Boolean {
-    TODO()
+    return student.grades.any { it.type == GradeType.A }
 }
 
 
 fun getStudentAges(students: List<Student>): List<Int> {
-    TODO()
+    return students.map { it.age }
 }
 
 fun getStudentsWithMinimumAge(students: List<Student>, minAge: Int): List<Student> {
-    TODO()
+    return students.filter { it.age >= minAge }
 }
 
 
 // gender == Gender.MALE
 // or gender.name == "MALE"
 fun countMaleStudents(students: List<Student>): Int {
-    TODO()
+    return students.count { it.gender == Gender.MALE }
 }
 
 
 // gender == Gender.FEMALE
 // or gender.name == "FEMALE"
 fun avgAgeOfFemaleStudent(students: List<Student>): Double {
-    TODO()
+    return students.asSequence().filter { it.gender == Gender.FEMALE }.map { it.age }.average()
 }
 
 fun getProductOfStudentAges(students: List<Student>): Int {
-    TODO()
+    return students.asSequence().map { it.age }.reduce { it, itTwo -> it + itTwo }
 }
 
 // ignore F Grades
 fun productOfStudentGrades(student: Student): Int {
-    TODO()
+    return student.grades.asSequence().filter { it.type != GradeType.F }.map { it.type.value }.reduce { it, itTwo -> it * itTwo }
 }
 
 // region BONUS
 
 // use maxByOrNull on grades
 fun getBestMathGradeFromStudent(student: Student): Grade? {
-    TODO()
+    return student.grades.asSequence().filter { it.subject == Subject.MATH }.maxByOrNull { it.type.value }
 }
 
 fun getSortedAges(students: List<Student>): List<Int> {
-    TODO()
+    return students.map { it.age }.sorted()
 }
 
 // endregion
